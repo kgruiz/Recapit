54d92d084cc6912787d56afef7e9ae3c9e64356f
kgruiz <kgruiz@umich.edu>
2025-02-20 20:33:53 -0500

    Working image to latex

--- main.py
+++ main.py
@@ -1,22 +1,56 @@
 import os
+import shutil
 from pathlib import Path
 
 import PIL.Image
 from google import genai
 from google.genai import types
+from pdf2image import convert_from_path
 
-IMAGE_PATH = Path("puppy.png")
 
-image = PIL.Image.open(IMAGE_PATH)
+def PDFToPNG():
 
-apiKey = os.getenv("GEMINI_API_KEY")
+    pdfPath = Path("465-Lecture-1.pdf")
 
-if apiKey is None:
-    raise ValueError("GEMINI_API_KEY environment variable not set")
+    images = convert_from_path(pdfPath)
 
-client = genai.Client(api_key=apiKey)
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents=["What is this image?", image]
-)
+    outputDir = Path(f"{pdfPath.stem}-pages")
 
-print(response.text)
+    if outputDir.exists():
+
+        shutil.rmtree(outputDir)
+
+    outputDir.mkdir(parents=True, exist_ok=True)
+
+    for i, image in enumerate(images):
+
+        image.save(Path(outputDir, f"{pdfPath.stem}-{i}.png"), "PNG")
+
+
+def ImageQuery():
+
+    # IMAGE_PATH = Path("puppy.png")
+
+    IMAGE_PATH = Path("465-Lecture-1-pages/465-Lecture-1-21.png")
+
+    image = PIL.Image.open(IMAGE_PATH)
+
+    apiKey = os.getenv("GEMINI_API_KEY")
+
+    if apiKey is None:
+        raise ValueError("GEMINI_API_KEY environment variable not set")
+
+    client = genai.Client(api_key=apiKey)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=["Transcribe this image, including all math, in latex format", image],
+    )
+
+    print(response.text)
+
+    Path("response.txt").write_text(response.text)
+
+
+if __name__ == "__main__":
+
+    ImageQuery()
